dd3af85c32a0d405bc2a717726e56953e85bd62e
wangjianling <wangjianling@tuandai.com>
2018-05-09 18:00:56 +0800

    add

--- server-consumer/src/main/java/com/springcloud/serverconsumer/controller/ConsumerController.java
+++ server-consumer/src/main/java/com/springcloud/serverconsumer/controller/ConsumerController.java
@@ -1,13 +1,16 @@
 package com.springcloud.serverconsumer.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
+
+    @Value("${url:http://172.22.1.120:8087}")
+    private String url;
 
 
     @Autowired
@@ -17,8 +20,8 @@ public class ConsumerController {
     @RequestMapping("/say")
     public String say(){
 
-        String msg = restTemplate.getForEntity("http://PROVIDER-SERVICE/hello",
-                String.class).getBody();
+        String msg = restTemplate.getForObject("http://provider-service/hello",
+                String.class);
 
     //    restTemplate.getForObject("");
         return "say:" + msg;
